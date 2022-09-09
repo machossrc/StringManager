84c036cb23a699f8382a1f8b845ebb5811dd1882
Admin <a01074287407@gmail.com>
2022-09-09 16:28:26 +0900

    포인터 변수로 CStringManager 를 받아서 저장되있던 m_pString 과 비교해주는 연산을 ==로 간추림

--- StringManager.h
+++ StringManager.h
@@ -5,6 +5,19 @@ class CStringManager
 private:
 	char* m_pString;
 public:
+	bool operator==(const CStringManager& rhs) const
+	{
+		if (m_pString != rhs.m_pString)
+		{
+			return false;
+		}
+		if (strcmp(m_pString, rhs.m_pString) != 0)
+		{
+			return false;
+		}
+		return true;
+	}
+public:
 	CStringManager(const char * szString);
 	CStringManager();
 	~CStringManager();
